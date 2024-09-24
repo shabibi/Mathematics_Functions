0d93c312433a04be0887771003a0bf83c13f163a
Afra Alshabibi <a.shabibi20@gmail.com>
2024-09-24 16:26:34 +0400

    Adding main function

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,8 +4,172 @@
     {
         static void Main(string[] args)
         {
+            bool flag = true;
+            int Menuchoice, ContnueChoise;
+            while (flag == true)
+            {
+                Console.Clear();
+                Console.WriteLine("choose a number: ");
+                Console.WriteLine(" 1. Factorial Calculator  ");
+                Console.WriteLine(" 2. Prime Number Checker  ");
+                Console.WriteLine(" 3. Fibonacci Sequence Generator  ");
+                Console.WriteLine(" 4. Greatest Common Divisor (GCD)   ");
+                Console.WriteLine(" 5. Sum of Digits ");
+                Console.WriteLine(" 6. Array Average  ");
+                Console.WriteLine(" 7. Count Vowels in a String  ");
+                Console.WriteLine(" 8. Temperature Converter  ");
+                Console.WriteLine(" 9. Word Count  ");
+                Console.WriteLine(" 10. String Capitalization  ");
+                // Menuchoice = int.Parse(Console.ReadLine());
+                Menuchoice = handelIntError(Console.ReadLine());
+                switch (Menuchoice)
+                {
+                    case 1:
+                        double num;
+                        Console.WriteLine("enter number: ");
+                        num = handelDoubleError(Console.ReadLine());
 
-            
+                        Console.WriteLine(CalculateFactoria(num));
+                        break;
+
+                    case 2:
+                        double in_num;
+                        bool is_prime;
+                        Console.WriteLine("enter number: ");
+                        in_num = handelDoubleError(Console.ReadLine());
+                        is_prime = IsPrime(in_num);
+
+                        if (is_prime == true)
+                        {
+                            Console.WriteLine("Tis numer is prime");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Tis numer is not prime");
+                        }
+                        break;
+
+                    case 3:
+
+                        Console.WriteLine("enter number: ");
+                        int fnum = handelIntError(Console.ReadLine());
+                        List<int> fibonacci = new List<int>();
+                        fibonacci = GenerateFibonacci(fnum);
+                        Console.Write("[");
+                        for (int i = 0; i < fibonacci.Count; i++)
+                        {
+                            Console.Write(fibonacci[i] + " ");
+                        }
+                        Console.WriteLine("]");
+                        break;
+
+                    case 4:
+                        Console.WriteLine("enter 2 numbers: ");
+                        int n1 = handelIntError(Console.ReadLine());
+                        int n2 = handelIntError(Console.ReadLine());
+
+                        Console.WriteLine("The greatest common number of "
+                            + n1 + " and " + n2 + " is " + GCD(n1, n2));
+
+                        break;
+
+                    case 5:
+                        int digits;
+                        Console.WriteLine("enter number: ");
+                        digits = handelIntError(Console.ReadLine());
+
+                        Console.WriteLine("the sum of digits is  " + SumOfDigits(digits));
+
+                        break;
+
+                    case 6:
+                        List<int> x_array = new List<int>();
+                        int input;
+                        Console.WriteLine("how many numbers you want to add? ");
+                        int count = handelIntError(Console.ReadLine());
+
+                        Console.WriteLine("enter your numbers: ");
+                        for (int i = 0; i < count; i++)
+                        {
+                            input = handelIntError(Console.ReadLine());
+                            x_array.Add(input);
+                        }
+                        Console.WriteLine(" the avrage is "
+                            + CalculateAverage(x_array));
+
+                        break;
+
+                    case 7:
+                        string world;
+                        Console.WriteLine("enter world ");
+                        world = Console.ReadLine();
+                        Console.WriteLine("the number of vowels litter in "
+                            + world + " is : " + CountVowels(world));
+
+                        break;
+
+                    case 8:
+                        double degree;
+                        Console.WriteLine("convert temperature to:  (enter 1 or 2)" +
+                            " \n 1.Celsius \n 2.Fahrenhei (enter 1 or 2)");
+                        int choice = handelIntError(Console.ReadLine());
+                        if (choice == 1)
+                        {
+                            Console.WriteLine("entet temperature dgree in Fahrenhei ");
+                            degree = handelDoubleError(Console.ReadLine());
+                            Console.WriteLine("The degree is : " + FahrenheitToCelsius(degree));
+                        }
+                        else if (choice == 2)
+                        {
+                            Console.WriteLine("entet temperature dgree in Celsius  ");
+                            degree = handelDoubleError(Console.ReadLine());
+                            Console.WriteLine("The degree is : " + CelsiusToFahrenheit(degree));
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid input..  ");
+                        }
+                        break;
+
+                    case 9:
+
+                        Console.WriteLine("Enter your sentence ");
+                        string inputSen = Console.ReadLine();
+                        Console.WriteLine("the number of world is : " + CountWords(inputSen));
+
+                        break;
+
+                    case 10:
+                        string inString;
+                        Console.WriteLine("Enter your sentance ");
+                        inString = Console.ReadLine();
+                        Console.WriteLine("Output : " + CapitalizeFirstLetter(inString));
+
+                        break;
+
+                    default:
+                        Console.WriteLine("Ivalid number.. ");
+                        break;
+
+
+
+                }
+                Console.WriteLine("Do you want to continue?(enter 1 or 2) \n 1.yes \n 2.no");
+                ContnueChoise = handelIntError(Console.ReadLine());
+                if (ContnueChoise == 1) { flag = true; }
+                else if (ContnueChoise == 2)
+                { flag = false; }
+                else
+                {
+                    Console.WriteLine("Ivalid number.. ");
+                    flag = false;
+                }
+
+            }
+
+
+
+
         }
         //1. Factorial Calculator function
         static double CalculateFactoria(double number)
